bbf9c7380e6780ce8f68163d72d21a39f2c6e43b
TheCraftsmen <castro.blas.martin@gmail.com>
2016-12-04 12:15:51 -0300

    commit inicial rama develop

--- config/routes.py
+++ config/routes.py
@@ -1,9 +1,9 @@
-from flask.ext.via.routers import default
+from flask.ext.via.routers.default import Pluggable as p
 from app.views import *
 
 urls = [
     # url, clase y nombre de la vista
-    default.Pluggable('/', Index, 'index'),
-    default.Pluggable('/about', About, 'about')
+    p('/', Index, 'index'),
+    p('/about', About, 'about')
 ]
 #
--- utilities/user/routes.py
+++ utilities/user/routes.py
@@ -1,5 +1,5 @@
-user_url = [default.Pluggable('/register',UserRegister,'register'),
-            default.Pluggable('/login',UserLogin,'login'),
-            default.Pluggable('/logout', UserLogout,'logout')]
+user_url = [p('/register',UserRegister,'register'),
+            p('/login',UserLogin,'login'),
+            p('/logout', UserLogout,'logout')]
 
 urls = urls + user_url
